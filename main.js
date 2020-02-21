01e3ebb59485dadb95baf9ea143f434dc76dcf8d
Quentin Gérome <q.gerome@whatever-company.com>
2020-02-21 16:08:11 +0100

    Updates

---
+++ main.js
@@ -0,0 +1,57 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* global document */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Editor } from 'slate-react'
+import PluginEditCode from '../lib/'
+
+import INITIAL_VALUE from './value'
+import renderBlock from './renderBlock'
+
+const plugin = PluginEditCode()
+const plugins = [plugin]
+
+class Example extends React.Component {
+  state = {
+    value: INITIAL_VALUE
+  }
+
+  ref = editor => {
+    this.editor = editor
+  }
+
+  onChange = ({ value }) => {
+    this.setState({
+      value
+    })
+  }
+
+  onToggleCode = () => {
+    const { value } = this.state
+    this.editor.toggleCodeBlock('paragraph').focus()
+  }
+
+  render() {
+    const { value } = this.state
+
+    return (
+      <div>
+        <button onClick={this.onToggleCode}>
+          {this.editor && this.editor.isInCodeBlock()
+            ? 'Paragraph'
+            : 'Code Block'}
+        </button>
+        <Editor
+          placeholder={'Enter some text...'}
+          plugins={plugins}
+          value={value}
+          ref={this.ref}
+          onChange={this.onChange}
+          renderBlock={renderBlock}
+        />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Example />, document.getElementById('example'))
---
+++ renderBlock.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function renderBlock(props) {
+  const { node, children, attributes } = props
+
+  switch (node.type) {
+    case 'code_block':
+      return (
+        <div className='code' {...attributes}>
+          {children}
+        </div>
+      )
+    case 'code_line':
+      return <pre {...attributes}>{children}</pre>
+    case 'paragraph':
+      return <p {...attributes}>{children}</p>
+    case 'heading':
+      return <h1 {...attributes}>{children}</h1>
+    default:
+      return null
+  }
+}
